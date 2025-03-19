cc7aafb7f54f64928b28343a7a37ad40149230ab
Qiyuan Zhuang <zqy66803@gmail.com>
2025-03-19 17:29:29 +0800

    q1b cost del

--- solvers/q1b_solver.py
+++ solvers/q1b_solver.py
@@ -41,11 +41,11 @@ def astar_initialise(problem: q1b_problem):
     food_grid = state.getFood()
     astarData.food_positions = tuple((x, y) for x in range(food_grid.width) for y in range(food_grid.height) if food_grid[x][y])
     
-    astarData.bfs_distances = precompute_bfs(state, astarData.food_positions)
+    # astarData.bfs_distances = precompute_bfs(state, astarData.food_positions)
 
     astarData.g_values[state.getPacmanPosition()] = 0  # 初始位置代价为 0
-    # astarData.queue.push((state, [], 0), astar_heuristic(state.getPacmanPosition(), astarData.food_positions))
-    astarData.queue.push((state, [], 0), astar_heuristic(state.getPacmanPosition(), astarData.bfs_distances))
+    astarData.queue.push((state, [], 0), astar_heuristic(state.getPacmanPosition(), astarData.food_positions))
+    # astarData.queue.push((state, [], 0), astar_heuristic(state.getPacmanPosition(), astarData.bfs_distances))
     
     return astarData
 
@@ -78,7 +78,7 @@ def astar_loop_body(problem: q1b_problem, astarData: AStarData):
     
     # if problem.isGoalState(state): # 这句话会变成False，可能是因为pacman在successor那里已经吃到了，自动更新为False
     if pacman_position in astarData.food_positions:
-        # print("----------- found one! ------------")
+        print("----------- found one! ------------")
         return  True, actions
 
     for successor, action, step_cost in problem.getSuccessors(state):
@@ -88,8 +88,8 @@ def astar_loop_body(problem: q1b_problem, astarData: AStarData):
         # 如果 successor 还没访问过，或者有更优路径，就更新
         if successor_position not in astarData.g_values or new_cost < astarData.g_values[successor_position]:
             astarData.g_values[successor_position] = new_cost
-            # priority = new_cost + astar_heuristic(successor_position, astarData.food_positions)
-            priority = new_cost + astar_heuristic(successor_position, astarData.bfs_distances)
+            priority = astar_heuristic(successor_position, astarData.food_positions)
+            # priority = new_cost + astar_heuristic(successor_position, astarData.bfs_distances)
             astarData.queue.update((successor, actions + [action], new_cost), priority)
 
     # for successor, action, step_cost in problem.getSuccessors(state):
@@ -103,50 +103,51 @@ def astar_loop_body(problem: q1b_problem, astarData: AStarData):
     return False, None
 
 
-# def astar_heuristic(pacman_position, food_positions):
-#     # YOUR CODE HERE
-#     if not food_positions:
-#         return 0
-#     return min(util.manhattanDistance(pacman_position, food) for food in food_positions)
-
-def astar_heuristic(pacman_position, bfs_distances):
-    # YOUR CODE HERE        
+def astar_heuristic(pacman_position, food_positions):
+    # YOUR CODE HERE
+    if not food_positions:
+        return 0
     
-    if pacman_position in bfs_distances:
-        return bfs_distances[pacman_position]
-    return min(util.manhattanDistance(pacman_position, food) for food in list(bfs_distances.keys())) 
+    return min(util.manhattanDistance(pacman_position, food) for food in food_positions)
 
-def precompute_bfs(state, food_positions):
-    """
-    预先计算每个点到food的距离，考虑wall
-    """
-    walls = state.getWalls()
-    width, height = walls.width, walls.height
-    bfs_distances = {}  
-    queue = util.Queue()
+# def astar_heuristic(pacman_position, bfs_distances):
+#     # YOUR CODE HERE        
     
-    for food in food_positions:
-        x, y = food
-        if walls[x-1][y] and walls[x][y-1] and walls[x][y+1] and walls[x+1][y]:
-            # print('not available')
-            continue
-            # bfs_distances[food] = float('inf')
-        bfs_distances[food] = 0
-        queue.push(food)
+#     if pacman_position in bfs_distances:
+#         return bfs_distances[pacman_position]
+#     return min(util.manhattanDistance(pacman_position, food) for food in list(bfs_distances.keys())) 
+
+# def precompute_bfs(state, food_positions):
+#     """
+#     预先计算每个点到food的距离，考虑wall
+#     """
+#     walls = state.getWalls()
+#     width, height = walls.width, walls.height
+#     bfs_distances = {}  
+#     queue = util.Queue()
     
-    while not queue.isEmpty():
-        x, y = queue.pop()
-        current_dist = bfs_distances[(x, y)]
+#     for food in food_positions:
+#         x, y = food
+#         if walls[x-1][y] and walls[x][y-1] and walls[x][y+1] and walls[x+1][y]:
+#             # print('not available')
+#             continue
+#             # bfs_distances[food] = float('inf')
+#         bfs_distances[food] = 0
+#         queue.push(food)
+    
+#     while not queue.isEmpty():
+#         x, y = queue.pop()
+#         current_dist = bfs_distances[(x, y)]
 
-        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # 四个方向
-            nx, ny = x + dx, y + dy
+#         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]: # 四个方向
+#             nx, ny = x + dx, y + dy
 
-            if 0 <= nx < width and 0 <= ny < height and not walls[nx][ny]:
-                if (nx, ny) not in bfs_distances: 
-                    bfs_distances[(nx, ny)] = current_dist + 1
-                    queue.push((nx, ny))
+#             if 0 <= nx < width and 0 <= ny < height and not walls[nx][ny]:
+#                 if (nx, ny) not in bfs_distances: 
+#                     bfs_distances[(nx, ny)] = current_dist + 1
+#                     queue.push((nx, ny))
 
-    return bfs_distances
+#     return bfs_distances
 
 
 # def astar_loop_body(problem: q1b_problem, astarData: AStarData):
@@ -195,4 +196,4 @@ def precompute_bfs(state, food_positions):
     
 #     return False, None
 
-# python pacman.py -l layouts/q1b_trickyCorners.lay -p SearchAgent -a fn=q1b_solver,prob=q1b_problem --timeout=5
+# python pacman.py -l layouts/q1b_bigCorners.lay -p SearchAgent -a fn=q1b_solver,prob=q1b_problem --timeout=5
